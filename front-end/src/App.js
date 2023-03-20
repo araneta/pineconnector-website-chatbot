66df81a6e899d6a1bf87d378d5a89f8d873bed9a
peakdev118 <peakdev118@gmail.com>
2023-03-20 01:31:20 -0400

    chatbot modify

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,11 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import Axios from 'axios';
 
 // import { FaFly } from 'react-icons/fa';
 
 import ChatComponent from './components/chat';
-import BotIcon from './assets/image/bot.png';
-// import LogoIcon from './assets/image/logo.png';
-// import HelloIcon from './assets/image/hello.png';
 
-import './App.css';
+import './assets/scss/app.scss';
 
 function SendMessage(data) {
 	return new Promise(async (resolve, reject) => {
@@ -26,14 +23,30 @@ function App() {
 	const [chatting_data, set_chatting_data] = useState('');
 	const [loading, setLoading] = useState(false);
 	const [messages, setMessages] = useState([]);
+	const messageBoxRef = useRef();
+	const textareaRef = useRef();
+	const bodyRef = useRef();
 
 	const onChangeMessage = (e) => {
-		set_chatting_data(e.target.value);
+		const meassageValue = e.target.value;
+		set_chatting_data(meassageValue);
+		changeInputHeight(meassageValue);
 	};
 
-	const onMessageKeyUp = (e) => {
-		if (e.which === 13) {
+	const changeInputHeight = (meassageValue) => {
+		// eslint-disable-next-line
+		const lineCount = (meassageValue.match(new RegExp('\n', 'g')) || []).length;
+		const height = 24 * (lineCount + 1);
+		textareaRef.current.style.height = height + 'px';
+		messageBoxRef.current.style.height = height + 16 + 'px';
+		bodyRef.current.style.paddingBottom = height + 30 + 'px';
+	};
+
+	const onMessageKeyDown = (e) => {
+		if (e.shiftKey === false && e.which === 13) {
+			e.stopPropagation();
 			e.preventDefault();
+			if (e.target.value === '') return false;
 			sendMessageSubmit();
 		}
 	};
@@ -72,6 +85,7 @@ function App() {
 			});
 
 		set_chatting_data('');
+		changeInputHeight('');
 	};
 
 	let messageData = messages.map((message, index) => {
@@ -92,74 +106,57 @@ function App() {
 	// };
 
 	return (
-		<div className="flex flex-col items-center justify-center w-screen min-h-screen bg-gray-100 text-gray-800 p-10 relative">
-			<div className="md:flex md:flex-col flex-grow w-full md:max-w-[40%] bg-white shadow-xl rounded-lg overflow-hidden">
-				{/* <div className="flex justify-center border-b-2 py-2">
-					<img className="h-[50px]" src={LogoIcon} alt="logo"></img>
-					<img className="h-[50px]" src={HelloIcon} alt="hello"></img>
-				</div> */}
-				<div className="flex flex-col flex-grow p-4 overflow-auto md:h-0 h-[calc(100vh-14rem)]">
-					<ChatComponent
-						humanChat={false}
-						chatContent={
-							<>
-								{/* <div>
-									<img src={HelloEmoticon} alt="hello"></img>
-								</div> */}
-								<span className="text-sm leading-none">
-									How are you? What can I help you? Please ask me.
-								</span>
-							</>
-						}
-					/>
-					{messageData}
-					{loading ? (
-						<div className="flex w-full mt-2 space-x-3 max-w-xs">
-							<div className="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300">
-								<img src={BotIcon} alt="bot"></img>
-							</div>
-							<div>
-								<span className="text-sm text-gray-500 leading-none">
-									typing...
-								</span>
-							</div>
-						</div>
-					) : null}
-				</div>
-				<div className="bg-gray-300 p-4 flex">
-					<input
-						className="flex items-center h-10 w-full rounded px-3 text-sm"
-						type="text"
-						placeholder="Type your message…"
-						onChange={onChangeMessage}
-						onKeyUp={onMessageKeyUp}
-						value={chatting_data}
-					/>
-					<button
-						onClick={sendMessageSubmit}
-						className="flex ml-1 h-10 items-center space-x-3 font-semibold bg-gradient-to-r from-blue-600 via-indigo-700 to-indigo-900 text-gray-100 rounded ring-2 ring-blue-200 px-6 py-2 hover:bg-white  hover:text-white hover:ring-slate-300"
-					>
-						<span className="text-white">send</span>
-						<span>
-							<svg
-								className="w-4 h-4 transform rotate-45 -mt-px"
-								fill="none"
-								stroke="currentColor"
-								viewBox="0 0 24 24"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									strokeWidth="2"
-									d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
-								></path>
-							</svg>
-						</span>
-					</button>
-				</div>
+		<>
+			<div
+				ref={bodyRef}
+				className="w-full min-h-screen bg-[#343541] scrollbar-thumb-slate-500/50 scrollbar-track-slate-500/[0.16] scrollbar scrollbar-w-[8px] scrollbar-thumb-rounded scrollbar-track-rounded"
+			>
+				<ChatComponent
+					humanChat={false}
+					chatContent={'How are you? What can I help you? Please ask me.'}
+				/>
+				{messageData}
+				{loading ? (
+					<ChatComponent humanChat={false} chatContent={'Typing...'} />
+				) : null}
 			</div>
-		</div>
+			<div
+				ref={messageBoxRef}
+				className="fixed px-3 py-2 bottom-3 md:w-[700px] w-[calc(100%-6rem)] md:left-[calc(50%-350px)] left-[3rem] dark dark:bg-gray-700 rounded-lg h-[40px] max-h-[216px]"
+			>
+				<textarea
+					ref={textareaRef}
+					// className="flex items-center h-10 w-full rounded px-3 text-sm resize-none max:h-[300px] py-3 bg-transparent"
+					className="max-h-[200px] m-0 w-full resize-none border-0 bg-transparent pr-6 focus:outline-0 focus:ring-0 focus-visible:ring-0 dark:bg-transparent text-white scrollbar-thumb-slate-500/50 scrollbar-track-slate-500/[0.16] scrollbar scrollbar-w-[8px] scrollbar-thumb-rounded scrollbar-track-rounded"
+					style={{ height: '24px' }}
+					placeholder="Type your message…"
+					onChange={onChangeMessage}
+					onKeyDownCapture={onMessageKeyDown}
+					value={chatting_data}
+				/>
+				<button
+					onClick={sendMessageSubmit}
+					className="absolute flex items-center justify-center p-1 rounded-md text-gray-500 bottom-2 right-6 hover:bg-gray-100 dark:hover:text-gray-400 dark:hover:bg-gray-900 disabled:hover:bg-transparent dark:disabled:hover:bg-transparent"
+				>
+					<span className="p-1">
+						<svg
+							className="w-[14px] transform rotate-45 mr-[-5px] mt-[-5px]"
+							fill="none"
+							stroke="currentColor"
+							viewBox="0 0 24 24"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+							></path>
+						</svg>
+					</span>
+				</button>
+			</div>
+		</>
 	);
 }
 
--- front-end/src/components/chat/index.js
+++ front-end/src/components/chat/index.js
@@ -6,28 +6,39 @@ function ChatComponent(props) {
 	return (
 		<>
 			{humanChat ? (
-				<div className="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
-					<div>
-						<div className="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
-							<p className="text-sm">{chatContent}</p>
+				<div className="group w-full text-gray-800 dark:text-gray-100 border-b border-black/10 dark:border-gray-900/50 bg-gray-50 dark:bg-[#444654]">
+					<div className="text-base gap-4 md:gap-6 md:max-w-2xl lg:max-w-2xl xl:max-w-3xl p-4 md:py-6 flex lg:px-0 m-auto">
+						<div className="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300">
+							<img
+								src={PersonIcon}
+								alt="person"
+								className="w-[80%] m-[10%]"
+							></img>
 						</div>
-					</div>
-					<div className="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300">
-						<img
-							src={PersonIcon}
-							alt="person"
-							className="w-[80%] m-[10%]"
-						></img>
+						<div>
+							<div className="text-white p-3 rounded-r-lg rounded-bl-lg">
+								<div className="text-sm">
+									{chatContent.split('\n').map((line, index) => {
+										return <div key={index}>{line}</div>;
+									})}
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			) : (
-				<div className="flex w-full mt-2 space-x-3 max-w-xs">
-					<div className="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300">
-						<img src={BotIcon} alt="bot"></img>
-					</div>
-					<div>
-						<div className="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
-							<p className="text-sm">{chatContent}</p>
+				<div className="group w-full text-gray-800 dark:text-gray-100 border-b border-black/10 dark:border-gray-900/50 dark:bg-[#343541]">
+					<div className="text-base gap-4 md:gap-6 md:max-w-2xl lg:max-w-2xl xl:max-w-3xl p-4 md:py-6 flex lg:px-0 m-auto">
+						<div className="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300">
+							<img src={BotIcon} alt="bot"></img>
+						</div>
+						<div>
+							<div className="p-3 rounded-r-lg rounded-bl-lg">
+								<div
+									className="text-sm text-gray-300"
+									dangerouslySetInnerHTML={{ __html: chatContent }}
+								/>
+							</div>
 						</div>
 					</div>
 				</div>
