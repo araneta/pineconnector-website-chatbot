04cd7dbad39a0d5b3cf0a1e8e472287cefd6f781
peakdev118 <peakdev118@gmail.com>
2023-04-13 12:41:55 -0400

    moidify front end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -58,7 +58,7 @@ function App() {
 	};
 
 	const sendMessageSubmit = () => {
-		if (e.target.value === '') return false;
+		if (chatting_data === '') return false;
 		setLoading(true);
 
 		let messageHistory = [
